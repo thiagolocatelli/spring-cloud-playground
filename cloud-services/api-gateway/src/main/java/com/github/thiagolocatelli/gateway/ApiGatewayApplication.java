addcfe97e68f6f695e76a5ac4307b8175d2562a9
Thiago Locatelli <thiago.locatellidasilva@transamerica.com>
2019-05-08 17:31:16 -0400

    Fixed broken import on api-gateway

--- cloud-services/api-gateway/src/main/java/com/github/thiagolocatelli/gateway/ApiGatewayApplication.java
+++ cloud-services/api-gateway/src/main/java/com/github/thiagolocatelli/gateway/ApiGatewayApplication.java
@@ -6,7 +6,6 @@ import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
 import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
 import org.springframework.context.annotation.Bean;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 public class ApiGatewayApplication {
