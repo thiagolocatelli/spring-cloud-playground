bf53ca3611ca601839787c3229b912e064d3d758
Thiago Locatelli <thiago.locatellidasilva@transamerica.com>
2019-05-08 09:39:03 -0400

    Created microservices infrastructure

---
+++ api-gateway/src/main/java/com/github/thiagolocatelli/gateway/ApiGatewayApplication.java
@@ -0,0 +1,22 @@
+package com.github.thiagolocatelli.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+	@Bean
+	public DiscoveryClientRouteDefinitionLocator clientRouteDefinitionLocator(DiscoveryClient discoveryClient, DiscoveryLocatorProperties properties) {
+		return new DiscoveryClientRouteDefinitionLocator(discoveryClient, properties);
+	}
+
+}
