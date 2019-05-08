95ec7232ae50653f8a96efe6bd88ba636eb15f22
Thiago Locatelli <thiago.locatellidasilva@transamerica.com>
2019-05-08 11:55:23 -0400

    Removed old license

--- rating-service/src/main/java/com/github/thiagolocatelli/rating/repository/RatingRepository.java
+++ rating-service/src/main/java/com/github/thiagolocatelli/rating/repository/RatingRepository.java
@@ -1,13 +1,3 @@
-/*
- * Copyright (c) 2015 Transamerica Corporation. ("Transamerica" or "us"). All Rights Reserved.
- *
- * This software is the confidential and proprietary information of
- * Transamerica ("Confidential Information").
- *
- * You shall not disclose such Confidential Information and shall use it only in
- * accordance with the terms of the license agreement you entered into
- * with Transamerica.
- */
 package com.github.thiagolocatelli.rating.repository;
 
 import com.github.thiagolocatelli.rating.domain.Rating;
