3f74095540882979c1771147de95a224348ed46b
Thiago Locatelli <thiago.locatellidasilva@transamerica.com>
2019-05-09 10:57:12 -0400

    Fixed groupId for micro-services and cloud-services and added support-libraries module

--- micro-services/cart-service/src/main/java/com/github/thiagolocatelli/cart/client/domain/Movie.java
+++ micro-services/cart-service/src/main/java/com/github/thiagolocatelli/cart/client/domain/Movie.java
@@ -1,4 +1,50 @@
 package com.github.thiagolocatelli.cart.client.domain;
 
 public class Movie {
+
+    private Long id;
+    private String name;
+    private Integer releaseYear;
+    private String synopsis;
+    private String poster;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getReleaseYear() {
+        return releaseYear;
+    }
+
+    public void setReleaseYear(Integer releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    public String getSynopsis() {
+        return synopsis;
+    }
+
+    public void setSynopsis(String synopsis) {
+        this.synopsis = synopsis;
+    }
+
+    public String getPoster() {
+        return poster;
+    }
+
+    public void setPoster(String poster) {
+        this.poster = poster;
+    }
 }
